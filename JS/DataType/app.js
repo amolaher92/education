78bac300b2bb807a13523773ea8d4ddbab1176d3
Amol_Aher <amolaher92@gmail.com>
2022-01-22 16:55:20 +0530

    Updated Array Methods.

---
+++ JS/DataType/app.js
@@ -0,0 +1,25 @@
+/**
+ * const myHeading = document.querySelector('#app');
+ * myHeading.textContent = "Hello World";
+ */
+
+//variable declration
+var Name = "Amol Aher"; //String
+let UndefinedData;  //undefined
+let age = 28; //Number
+const gender = true; //Boolean
+//const hobbies = new Array("Development", "Reading", "Cooking"); //Array
+const hobbies = ["Development", "Reading", "Cooking"]; //Array
+const education = {
+    MCS: "Master Computer Science",
+    BCS: "Bacholer Computer Science",
+}; //Object
+
+console.log(`My name is ${Name}. I am ${age} year old. I am ${gender}.`);
+console.log(`my hobbies are ${hobbies}`);
+console.log(`My education are ${education.MCS} and ${education.BCS}`);
+
+const dataType = [Name, age, gender, hobbies, education,UndefinedData];
+dataType.forEach((typeData) => {
+    console.log(typeof(typeData));
+});
