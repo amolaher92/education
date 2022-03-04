5393aacdc680dd6c17e1888293c73c29b03e167f
Amol_Aher <amolaher92@gmail.com>
2022-03-04 17:05:16 +0530

    update db

--- Interview/main.js
+++ Interview/main.js
@@ -37,6 +37,20 @@ const Bubbleshorts = {
             if (arr[i] + arr[i - 1] + arr[i - 2] === 7) return true
         }
         return false
+    },
+    //multiple array
+    /* sumOfArray(arr) {
+         let sum = 0
+         for (let i = 0; i < arr.length; i++) {
+             let marr = arr[i].length
+             for (let j =0; j < marr; i++) {
+                 sum += arr[i][j]
+             }
+         }
+         return sum
+     },*/
+    sumArrayReduce(arr) {
+        return arr.reduce((t, e) => t.concat(e)).reduce((t, e) => t + e)
     }
 
 }
@@ -45,4 +59,19 @@ const myArr = [34, 23, 56, 12, 67]
 console.log(obj.bubblesort(myArr))
 console.log(obj.bubbleRsort(myArr))
 let amArr = [2, 1, 5, 1, 0]
+let marr = [[1, 2], [1, 5], [4, 6], [6, 8]]
 console.log(obj.luckySeven(amArr))
+//console.log(obj.sumOfArray(marr))
+console.log(obj.sumArrayReduce(marr))
+
+//simple callback
+function testCallback(arr, callback) {
+    console.log(arr);
+    callback(arr);
+}
+
+testCallback(marr, function (arr) {
+    console.log("Callback is call")
+    console.log(typeof(arr))
+})
+
